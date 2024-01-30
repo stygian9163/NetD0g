dd6be3d416cfcbd8adb588b717efdfde46fa03a7
Waleed Tariq <waleedtariq@Waleeds-MacBook-Pro.local>
2024-01-30 18:58:32 +0400

    adding interface selection

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from scapy.all import sniff
+import psutil   # for the network interfaces
+
+# to find and print network interfaces
+addrs = psutil.net_if_addrs()
+interface_names = list(addrs.keys())
+
+print("Network Interfaces:")
+print(interface_names)
+
+interface = input("Enter the interface you would like to sniff on : ")
 
 # Sniff Beacon frames on the "en0" interface in monitor mode
-a = sniff(iface="en0", prn=lambda x: x.summary(), filter="tcp", count=3)
+a = sniff(iface=interface, prn=lambda x: x.summary(), filter="tcp", count=3)
 print(a)
-
-
