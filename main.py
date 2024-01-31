3580941286a6ff6c5f803be07e547ea097ea243b
Waleed Tariq <waleedtariq@Waleeds-MacBook-Pro.local>
2024-01-31 11:50:09 +0400

    added interface,filter and count selection

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import psutil   # for the network interfaces
 # to find and print network interfaces
 addrs = psutil.net_if_addrs()
 interface_names = list(addrs.keys())
-
 print("Network Interfaces:")
 print(interface_names)
-
 interface = input("Enter the interface you would like to sniff on : ")
+fil = input(">> Filter (tcp and port 60) : ")
+count = int(input(">> Packet count to capture (default 3): "))
+
 
 # Sniff Beacon frames on the "en0" interface in monitor mode
-a = sniff(iface=interface, prn=lambda x: x.summary(), filter="tcp", count=3)
+a = sniff(iface=interface, prn=lambda x: x.summary(), filter=fil, count=count)
 print(a)
